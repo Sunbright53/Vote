705ba4d5a247177e7b77d2fc7398e28032493b76
Pranai phoprasat <pranai.pp5698@gmail.com>
2025-10-06 17:22:00 +0700

    first commit

--- poll-app/src/main.tsx
+++ poll-app/src/main.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-
-import './styles.css'; // ถ้าต้องการ
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Poll-Vote">
+    {/* ใช้ค่าอัตโนมัติตาม Vite:
+       - dev = '/'
+       - build (GitHub Pages) = '/Poll-Vote/' ตาม vite.config.ts */}
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
-)
+);
